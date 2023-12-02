d051847bb7ebf65e40567a528480521e726e4fed
nainghtetlinn <naing.work.it@gmail.com>
2023-12-02 13:00:49 +0630

    add middleware for protecting routes

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { withAuth, NextRequestWithAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+
+export default withAuth(
+  (req: NextRequestWithAuth) => {
+    const token = req.nextauth.token
+    console.log(token)
+    const pathname = req.nextUrl.pathname
+
+    if (
+      pathname.startsWith('/dashboard') &&
+      token?.role !== 'ADMIN' &&
+      token?.role !== 'SUPER_ADMIN'
+    ) {
+      return NextResponse.rewrite(new URL('/denied', req.url))
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+)
+
+export const config = {
+  matcher: ['/dashboard'],
+}
