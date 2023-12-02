b6ea8a630f1908360c4ba8beff15d1918ce42f12
nainghtetlinn <naing.work.it@gmail.com>
2023-12-02 11:38:45 +0630

    update user validation

--- validations/userValidation.ts
+++ validations/userValidation.ts
@@ -1,13 +1,16 @@
 import { z } from 'zod'
 
 export const registerUserSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string().min(5),
-  phone_no: z.number(),
+  name: z.string().min(1, 'Name required.').max(30),
+  email: z.string().min(1, 'Email required.').email(),
+  password: z.string().min(5, 'Password must be at least 5 characters.'),
+  phone_no: z
+    .string()
+    .min(1, 'Phone number required.')
+    .max(15, 'Invalid phone number.'),
 })
 
 export const loginUserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(5),
+  email: z.string().min(1, 'Email required.').email(),
+  password: z.string().min(5, 'Password must be at least 5 characters.'),
 })
