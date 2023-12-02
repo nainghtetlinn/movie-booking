4c573420790a35a5f486f1648567df7aa7a95c60
nainghtetlinn <naing.work.it@gmail.com>
2023-12-02 09:40:02 +0630

    create login form

---
+++ app/login/form.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import {
+  TextField,
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
+  Link,
+  Button,
+} from '@mui/material'
+import NextLink from 'next/link'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { loginUserSchema } from '@/validations/userValidation'
+
+const Form = () => {
+  const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ email: string; password: string }>({
+    resolver: zodResolver(loginUserSchema),
+  })
+
+  const onSubmit = async (e: any) => {
+    try {
+      const res = await signIn('credentials', { ...e, redirect: false })
+      if (res?.ok) {
+        router.push('/')
+        router.refresh()
+      } else {
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <Card sx={{ maxWidth: 400 }}>
+      <CardHeader
+        title='Login'
+        titleTypographyProps={{ textAlign: 'center' }}
+      />
+      <CardContent>
+        <Box component='form' onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            {...register('email')}
+            label='Email'
+            placeholder='example@gmail.com'
+            fullWidth
+            size='small'
+            margin='dense'
+            helperText={errors.email?.message}
+            error={!!errors.email?.message}
+          />
+          <TextField
+            {...register('password')}
+            label='Password'
+            fullWidth
+            size='small'
+            margin='dense'
+            helperText={errors.password?.message}
+            error={!!errors.password?.message}
+          />
+          <div className='flex items-center justify-between mt-4'>
+            <Link href='/register' component={NextLink}>
+              Don't have an account?
+            </Link>
+            <Button type='submit' variant='contained'>
+              Login
+            </Button>
+          </div>
+        </Box>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Form
---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import Form from './form'
+
+const LoginPage = () => {
+  return (
+    <div className='min-h-[80vh] flex justify-center items-center'>
+      <Form />
+    </div>
+  )
+}
+
+export default LoginPage
