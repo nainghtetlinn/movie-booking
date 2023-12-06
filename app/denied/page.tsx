93f4d6f10affd486622dafe79b3fad6f298f5c84
nainghtetlinn <naing.work.it@gmail.com>
2023-12-06 22:36:54 +0630

    change access denied page

--- app/denied/page.tsx
+++ app/denied/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Typography } from '@mui/material'
+import Link from 'next/link'
 
 const DeniedPage = () => {
-  return <div>DeniedPage</div>
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <div className='flex flex-col items-center'>
+        <Typography variant='h3'>403 | Access denied</Typography>
+        <Link href='/'>Go to home</Link>
+      </div>
+    </div>
+  )
 }
 
 export default DeniedPage
