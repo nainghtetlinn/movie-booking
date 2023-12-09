28ae9974a03e47029dce2b891a29ae05cd0c95de
nainghtetlinn <naing.work.it@gmail.com>
2023-12-09 13:26:02 +0630

    create new movie schema

---
+++ validations/movieValidation.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const newMovieSchema = z.object({
+  title: z.string().min(1, 'Title required.').max(255),
+  description: z.string(),
+  poster_url: z.string().min(1, 'Poster url required.'),
+  director: z.string(),
+  actors: z.string(),
+  duration_min: z.number().min(1, 'Duration required.'),
+  release_date: z.string().datetime(),
+  genres: z.array(z.string()),
+})
