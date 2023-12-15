a07dde974760bc719bf180475f0cb67277f98a20
nainghtetlinn <naing.work.it@gmail.com>
2023-12-15 17:49:07 +0630

    update movie schema

--- validations/movieValidation.ts
+++ validations/movieValidation.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod'
 
-export const newMovieSchema = z.object({
+export const MovieInputSchema = z.object({
   title: z.string().min(1, 'Title required.').max(255),
-  description: z.string().optional(),
+  description: z.string().optional().nullable(),
   poster_url: z.string(),
-  director: z.string().optional(),
-  actors: z.string().optional(),
+  director: z.string().optional().nullable(),
+  actors: z.string().optional().nullable(),
   duration_min: z.coerce.number().min(1, 'Duration required.'),
   release_date: z.coerce.date(),
-  genres: z.string().optional(),
+  genres: z.string().optional().nullable(),
 })
 
-export type NewMovieType = z.infer<typeof newMovieSchema>
+export type TMovie = z.infer<typeof MovieInputSchema>
