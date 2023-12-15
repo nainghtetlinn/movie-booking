8adcd1bb696e6bad78cb4b1ba11cf6330acc136b
nainghtetlinn <naing.work.it@gmail.com>
2023-12-15 18:27:45 +0630

    create update movie route

--- app/api/movie/[id]/route.ts
+++ app/api/movie/[id]/route.ts
@@ -1,8 +1,9 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getServerSession } from 'next-auth'
-import { options } from '../../auth/[...nextauth]/options'
 import prisma from '@/prisma/prismaClient'
 import response from '@/utils/response'
+import { MovieInputSchema } from '@/validations/movieValidation'
+import { getServerSession } from 'next-auth'
+import { NextRequest, NextResponse } from 'next/server'
+import { options } from '../../auth/[...nextauth]/options'
 
 export async function DELETE(
   req: NextRequest,
@@ -23,6 +24,43 @@ export async function DELETE(
 
     await prisma.movie.delete({ where: { id } })
     return NextResponse.json(response.success({ id, message: 'Deleted.' }))
+  } else {
+    return NextResponse.json(response.error('Denied.'), { status: 403 })
+  }
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id
+  const session = await getServerSession(options)
+  const isAdmin =
+    session?.user.role === 'ADMIN' || session?.user.role === 'SUPER_ADMIN'
+
+  const body = await req.json()
+
+  const validation = MovieInputSchema.safeParse(body)
+
+  if (!validation.success)
+    return NextResponse.json(
+      response.error('Invalid inputs.', validation.error.format()),
+      { status: 400 }
+    )
+
+  if (isAdmin) {
+    const foundMovie = await prisma.movie.findUnique({ where: { id } })
+    if (!foundMovie) {
+      return NextResponse.json(response.error('Movie not found.'), {
+        status: 404,
+      })
+    }
+
+    const updatedMovie = await prisma.movie.update({
+      where: { id },
+      data: validation.data,
+    })
+    return NextResponse.json(response.success(updatedMovie))
   } else {
     return NextResponse.json(response.error('Denied.'), { status: 403 })
   }
