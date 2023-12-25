68f1991e413de73115b6811e348f313fa689c7f6
nainghtetlinn <naing.work.it@gmail.com>
2023-12-25 22:58:21 +0630

    update show validation schema

--- validations/showValidation.ts
+++ validations/showValidation.ts
@@ -3,7 +3,13 @@ import { z } from 'zod'
 export const ShowInputSchema = z.object({
     start_time: z.coerce.date(),
     end_time: z.coerce.date(),
+    date: z.coerce.date(),
     movieId: z.string().min(1, 'Movie id required.'),
-}).refine(({ start_time, end_time, }) => start_time < end_time, { message: "Invalid inputs." })
+})
+    .refine(({ start_time, end_time, }) => {
+        const totalStart = start_time.getTime()
+        const totalEnd = end_time.getTime()
+        return totalEnd > totalStart
+    }, { message: "Invalid inputs." })
 
 export type TShow = z.infer<typeof ShowInputSchema>
