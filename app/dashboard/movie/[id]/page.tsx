fa7606252c661cad51a0a9e1e7c1b87c88ba7900
nainghtetlinn <naing.work.it@gmail.com>
2023-12-28 21:25:56 +0630

    redirect if movie or show not found when editing

--- app/dashboard/movie/[id]/page.tsx
+++ app/dashboard/movie/[id]/page.tsx
@@ -1,9 +1,15 @@
 import prisma from '@/prisma/prismaClient'
 import { Typography } from '@mui/material'
+import { redirect } from 'next/navigation'
 import Form from './form'
 
 const EditMoviePage = async ({ params }: { params: { id: string } }) => {
   const movie = await prisma.movie.findUnique({ where: { id: params.id } })
+
+  if (!movie) {
+    redirect('/dashboard/movie')
+  }
+
   return (
     <div>
       <Typography variant='h4' sx={{ mb: 1 }}>
--- app/dashboard/show/[id]/page.tsx
+++ app/dashboard/show/[id]/page.tsx
@@ -1,5 +1,6 @@
 import prisma from '@/prisma/prismaClient'
 import { Typography } from '@mui/material'
+import { redirect } from 'next/navigation'
 import Form from './form'
 
 const EditShowPage = async ({ params }: { params: { id: string } }) => {
@@ -7,6 +8,11 @@ const EditShowPage = async ({ params }: { params: { id: string } }) => {
   const movies = await prisma.movie.findMany({
     select: { id: true, title: true },
   })
+
+  if (!show) {
+    redirect('/dashboard/show')
+  }
+
   return (
     <div>
       <Typography variant='h4' sx={{ mb: 1 }}>
