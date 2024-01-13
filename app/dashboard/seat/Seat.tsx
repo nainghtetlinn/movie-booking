eafb399005331cfc735fd5845809fa8fbc828413
nainghtetlinn <naing.work.it@gmail.com>
2024-01-13 09:26:20 +0630

    add seat color according to seat status

--- app/dashboard/seat/Seat.tsx
+++ app/dashboard/seat/Seat.tsx
@@ -1,4 +1,4 @@
-import { Seat } from '@prisma/client'
+import { Seat, SeatStatus } from '@prisma/client'
 
 type SeatWithRowName = {
   rowName: string
@@ -6,8 +6,12 @@ type SeatWithRowName = {
 }
 
 const Seat = ({ rowName, seat }: SeatWithRowName) => {
+  const seatColor = getSeatColor(seat.status)
+
   return (
-    <div className='w-8 h-8 rounded bg-gray-300 text-sm flex items-center justify-center'>
+    <div
+      className={`w-8 h-8 rounded text-sm flex items-center justify-center ${seatColor}`}
+    >
       {rowName}
       {seat.number}
     </div>
@@ -15,3 +19,19 @@ const Seat = ({ rowName, seat }: SeatWithRowName) => {
 }
 
 export default Seat
+
+function getSeatColor(status: SeatStatus) {
+  let color = ''
+  switch (status) {
+    case 'PURCHASED':
+      color = 'bg-red-400'
+      break
+    case 'NOT_AVAILABLE':
+      color = 'bg-gray-300'
+      break
+    case 'AVAILABLE':
+      color = 'bg-green-300'
+      break
+  }
+  return color
+}
